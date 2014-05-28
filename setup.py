303617475f02d315e11e1f84f3ab4729a2a11a8b
Calvin Belden <cbelden@nd.edu>
2014-05-28 13:37:35 -0500

    Fixed erroneously named setup parameter.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='lyricscrambler',
       author='Cal Belden',
       author_email='calvinbelden@gmail.com',
       packages=['lyricscramble'],
-      require_installs=['markovchain',
+      install_requires=['markovchain',
                         'lyricfetch'],
       dependency_links=['git+git://github.com/cbelden/markovchain.git@master',
                         'git+git://github.com/cbelden/lyricfetch.git@master'])
