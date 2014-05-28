6cde494a438d06270e04d6f582d74125aa7fc7a7
Calvin Belden <cbelden@nd.edu>
2014-05-28 15:26:53 -0500

    Added lyricfetch and markovchain packages directly into project.

---
+++ lyricscramble/lyricfetch/lyric_client.py
@@ -0,0 +1,110 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+class LyricClient():
+    """The Lyric Client retrieves lyrics from Lyric Wikia's API."""
+
+    def __init__(self):
+        """Initializes the Lyric Client."""
+        pass
+
+    def _get_lyric_url(self, artist, title):
+        """Retrieves the url to the webpage that stores the lyrics for the requested song."""
+
+        url_params = {'artist': artist, 'song': title, 'fmt': 'html'}
+
+        try:
+            r = requests.get("http://lyrics.wikia.com/api.php", params=url_params)
+        except Exception, e:
+            raise e
+
+        # Return empty string if lyric request failed
+        if r.status_code is not 200:
+            return None
+
+        # Read and parse the response
+        html = r.text
+        soup = BeautifulSoup(html)
+
+        # The first anchor in the response directs to the lyrics page
+        # TODO: make this more robost. if lyric wikia changes their api response, this service might break. -Cal
+        link = soup.find('a')
+
+        if link:
+            return link['href']
+        else:
+            return None
+
+    def _extract_lyrics(self, lyric_url):
+        """Extracts the lyrics from a Lyric Wikia lyrics page."""
+
+        # Parse lyric url response
+        r = requests.get(lyric_url)
+        soup = BeautifulSoup(r.text)
+
+        # Locate div that stores the lyrics
+        lyricbox = soup.find(class_='lyricbox')
+
+        # Return empty string if they lyrics are missing
+        if not lyricbox:
+            return ''
+
+        # Extract and store lyrics
+        lyrics = ''
+        last_tag = None
+
+        for content in lyricbox.contents:
+
+            # Within the lyricbox, the first element(s) consist of an ad contained within
+            # a div. Additionally, each line from the lyrics is split by a <br> block. To
+            # boot, after the last lyric line, there is a <p> block with some metrics.
+            # The div and br blocks have non-None .name values, but both the NavigableText
+            # and the <p> tags both have None values for the .name attribute.
+            #
+            # The following works by assuming that the first non-lyric elements will return
+            # a non-None value for name, and the lyrics (text) will always return None. The
+            # only time two consecutive None values will occur after the last lyric has been
+            # read and the current element is the non-important <p> block. We can break at
+            # this point.
+            # TODO: follow up with this: if we can get the <p> block to have a non-None
+            # .name attribute, we can clean this up quite a bit.
+
+            tag = content.name
+
+            # If last two tags are None, assume we've read all the lyrics
+            if not tag and not last_tag:
+                break
+
+            # Append content if there is no tag (ie. the element is plain text)
+            if not tag:
+
+                # Skip line if it's contained in brackets
+                if re.match('\[.+\]', content):
+                    continue
+
+                lyrics += content + '\n'
+
+            # Update the last seen tag
+            last_tag = tag
+
+        return lyrics
+
+    def get_lyrics(self, artist, title):
+        """Retrieves the lyrics for a given song using the Lyric Wikia website."""
+
+        # Unfortunately, when we submit a song lyric request using Lyric Wikia's API,
+        # the response contains only a snippet of the song lyrics. A url is also provided,
+        # however, for a page that contains the full lyric listing. To retrieve the lyrics,
+        # we must get this url, visit the page, and parse the contents for the lyric
+        # listing. -Cal
+
+        lyric_url = self._get_lyric_url(artist, title)
+        return self._extract_lyrics(lyric_url)
+
+
+if __name__ == '__main__':
+
+    client = LyricClient()
+    print client.get_lyrics('Drake', 'Marvins room')
---
+++ lyricscramble/markovchain/markov_chain.py
@@ -0,0 +1,113 @@
+import string
+import random
+
+
+class MarkovChain(object):
+
+    def __init__(self, text):
+        """Generates the underlying Markov Chain."""
+
+        random.seed()                                           # set seed based on current time
+        self._markov_chain = self._construct_chain(text)        # the underlyng markov chain
+        self._stop_p = 0.07                                     # set probabality that a generated phrase ends
+
+    def __iter__(self):
+        """Iterates over the terms in the underlying Markov Chain."""
+
+        for term, consecutive_terms in self._markov_chain.iteritems():
+            yield term, consecutive_terms
+
+    def __getitem__(self, key):
+        """Returns the bigrams for a given term."""
+
+        return self._markov_chain[key]
+
+    def _construct_chain(self, text):
+        """Constructs the underlying Markov Chain."""
+
+        if not text:
+            raise Exception('No text to create the Markov Chain.')
+
+        words = self._extract_words(text)
+        bigrams = {}
+
+        for i in range(len(words) - 1):
+
+            # Get bigram
+            first = words[i]
+            second = words[i+1]
+
+            # Add the current word-pair to bigrams
+            if not first in bigrams:
+                bigrams[first] = {}
+            if not second in bigrams[first]:
+                bigrams[first][second] = 0
+
+            # Increment the bigram count
+            bigrams[first][second] += 1
+
+        return bigrams
+
+    def _extract_words(self, s):
+        """Strips input string of punctuation and splits on whitespace."""
+
+        stripped = self._strip_punctuation(s)
+        return stripped.split()
+
+    def _strip_punctuation(self, s):
+        """Strips punctuation from input string and sets all characters to lowercase."""
+
+        # Convert to utf-8
+        s = s.encode('utf-8', 'replace')
+
+        # Strip punctuation
+        table = string.maketrans("", "")
+        stripped = s.translate(table, string.punctuation)
+
+        return stripped.lower()
+
+    def generate_phrase(self):
+        """Generates a phrase based on the lyric corpus."""
+
+        new_word = ''
+        msg = ''
+
+        while 1:
+
+            # Generate a successor word
+            if new_word in self._markov_chain:
+                new_word = self._get_next_term(self._markov_chain[new_word])
+
+            # Generate a random word if new_word is not a key in the markov chain
+            else:
+                new_word = random.choice(self._markov_chain.keys())
+
+            # append successor to word
+            msg += new_word + ' '
+
+            # probabalistically decide to strop
+            if random.random() <= self._stop_p:
+                break
+
+        return msg.capitalize()
+
+    def _get_next_term(self, tf):
+        """Stochastically chooses the next generated term."""
+
+        total_occurences = 0
+        count = 0
+
+        for term in tf:
+            total_occurences += tf[term]
+
+        # will be used to "randomly" return one of the options
+        idx = random.randrange(total_occurences)
+
+        # Return one of the words based on idx. Words with a higher requency
+        # will be chosen more often
+        for term, freq in tf.iteritems():
+            count += freq
+            if idx <= count:
+                return term
+
+        raise Exception('Error: could not generate a successor term.')
