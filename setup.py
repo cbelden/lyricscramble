8872eae50253c6733598de3df3c9886d3aa8b1c7
Calvin Belden <cbelden@nd.edu>
2014-05-28 14:18:35 -0500

    Created simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='lyricscramble',
+      version='1.0',
+      description='Generates silly phrases from song lyrics.',
+      author='Cal Belden',
+      author_email='calvinbelden@gmail.com',
+      packages=['lyricscramble'])
