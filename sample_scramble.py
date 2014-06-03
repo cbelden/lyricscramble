ed0159c8766c5950285f2e35c971d19cbf09e627
Calvin Belden <cbelden@nd.edu>
2014-06-03 11:24:05 -0500

    No need to call the _update_chain method; LyricScrambler now performs this automatically.

--- sample_scramble.py
+++ sample_scramble.py
@@ -4,13 +4,10 @@ from lyricscramble import LyricScrambler
 scrambler = LyricScrambler()
 songs = [('drake', 'marvins room'), ('drake', 'all me'), ('drake', 'bull shit song')]
 
-# Get corpus
+# Create corpus
 for song in songs:
     if not scrambler.add_song(song[0], song[1]):
         print "Couldn't find: " + str(song)
-
-# Update underlying Markov chain (what generates the phrases)
-scrambler.update_chain()
 
 # Generate phrases
 for i in range(10):
